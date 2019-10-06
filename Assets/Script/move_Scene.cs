3f43408afa06568d993d9516e636b209bb4c09f3
onenechan <n12644748@gmail.com>
2019-10-06 22:32:28 +0900

    Scene移動を実装

---
+++ Assets/Script/move_Scene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class move_Scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void H2Abutton() {
+        SceneManager.LoadScene("game");
+    }
+    public void EpsilonButton() {
+        SceneManager.LoadScene("game_epsilon");
+    }
+}
