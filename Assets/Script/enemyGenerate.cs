bc74b543cd9f6a962c2d096ec579a1a302f96666
onenechan <n12644748@gmail.com>
2019-10-07 19:11:08 +0900

    Add enemies and the enemy generator

---
+++ Assets/Script/enemyGenerate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyGenerate : MonoBehaviour {
+
+    public GameObject enemy;
+    float timer;
+    float interval = 3.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > interval) {
+            transform.position = new Vector2(Random.Range(-5.0f, 5.0f), transform.position.y);
+            Instantiate(enemy, transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
